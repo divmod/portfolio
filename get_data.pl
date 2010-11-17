fa637858ea631361a786f0183467a4f923eaaacd
divmod <joydivasian@gmail.com>
2010-11-17 00:04:58 -0600

    Commented out call to gnuplot in get_data.pl

--- get_data.pl
+++ get_data.pl
@@ -62,13 +62,13 @@ $exec .= ">_plot.in" if $plot;
 
 system $exec;
 
-if ($plot) {
-  open(GNUPLOT, "|gnuplot");
-  GNUPLOT->autoflush(1);
-  print GNUPLOT "set title '$symbol'\nset xlabel 'time'\nset ylabel 'data'\n";
-  print GNUPLOT "plot '_plot.in'\n";
-  STDIN->autoflush(1);
-  <STDIN>;
-}
+#if ($plot) {
+#  open(GNUPLOT, "|gnuplot");
+#  GNUPLOT->autoflush(1);
+#  print GNUPLOT "set title '$symbol'\nset xlabel 'time'\nset ylabel 'data'\n";
+#  print GNUPLOT "plot '_plot.in'\n";
+#  STDIN->autoflush(1);
+#  <STDIN>;
+#}
 
 
